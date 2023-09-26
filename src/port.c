d0ca199b6793794e1ee7563d577fb48209f622ec
hanyuan-wsl <2524395907@qq.com>
2023-09-26 20:41:58 +0800

    fix: the leading zero debug output

--- src/port.c
+++ src/port.c
@@ -9,13 +9,13 @@
 
 uint32_t mmio_read_32(uintptr_t addr) {
 #ifdef HOST_DEBUG
-    printf("Trying to read memory @ 0x%8lx\n", addr);
+    printf("Trying to read memory @ 0x%08lx\n", addr);
 #endif
     return 0;
 }
 
 void mmio_write_32(uintptr_t addr, uint32_t f_val) {
 #ifdef HOST_DEBUG
-    printf("Trying to write memory @ 0x%8lx value 0x%8lx\n", addr, f_val);
+    printf("Trying to write memory @ 0x%08lx value 0x%8lx\n", addr, f_val);
 #endif
 }
