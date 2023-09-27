a3cbf76fdeb75f882b5246fe03914c6aa0ee46ab
hanyuan <2524395907@qq.com>
2023-09-27 18:31:44 +0800

    fix: set the mem to shared

--- src/port.c
+++ src/port.c
@@ -16,7 +16,7 @@ int pinmux_setup(uintptr_t base_addr) {
         printf("Failed to open /dev/mem. Are you root?\n");
         return -1;
     }
-    pinmux_base = mmap(0, 0x1000, PROT_READ | PROT_WRITE, MAP_PRIVATE, fd, base_addr);
+    pinmux_base = mmap(0, 0x1000, PROT_READ | PROT_WRITE, MAP_SHARED, fd, base_addr);
     if (pinmux_base == NULL) {
         printf("Failed to get the pinmux register base address\n");
         close(fd);
@@ -32,17 +32,9 @@ void pinmux_close() {
 }
 
 uint32_t pinmux_read_32(uintptr_t addr) {
-#ifdef HOST_DEBUG
-    printf("Trying to read memory @ 0x%08lx\n", (uintptr_t) pinmux_base + addr);
-    return 0;
-#endif
     return *((volatile uint32_t *) (pinmux_base + addr));
 }
 
 void pinmux_write_32(uintptr_t addr, uint32_t f_val) {
-#ifdef HOST_DEBUG
-    printf("Trying to write memory @ 0x%08lx value 0x%08x\n", (uintptr_t) pinmux_base + addr, f_val);
-    return;
-#endif
     *((volatile uint32_t *) (pinmux_base + addr)) = f_val;
 }
